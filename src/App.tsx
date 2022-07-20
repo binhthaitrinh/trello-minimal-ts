98f9f933c87e9f2daadec19c250edc4558b6d280
Kyle Trinh <kyle.trinh94@gmail.com>
2022-07-19 21:34:57 -0700

    :sparkles: Introduce AppStateContext to store global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { AddNewItem } from './AddNewItem';
 import { Column } from './Column';
+import { useAppState } from './state/AppStateContext';
 import { AppContainer } from './style';
 
 function App() {
+  const { lists } = useAppState();
+
   return (
     <AppContainer>
-      <Column text='Todo:' />
+      {/* We don't have to specify the type of `list` since TS derives it automatically */}
+      {lists.map((list) => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log} />
     </AppContainer>
   );
---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,70 @@
+import { createContext, useContext } from 'react';
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: '0',
+      text: 'To Do',
+      tasks: [{ id: 'c0', text: 'Generate app scaffold' }],
+    },
+    {
+      id: '1',
+      text: 'In Progress',
+      tasks: [{ id: 'c2', text: 'Learn Typescript' }],
+    },
+    {
+      id: '2',
+      text: 'Done',
+      tasks: [{ id: 'c3', text: 'Begin to use static typing' }],
+    },
+  ],
+};
+
+type AppStateContextProps = {
+  lists: List[];
+  getTasksByListId(id: string): Task[];
+};
+
+const AppStateContext = createContext<AppStateContextProps>(
+  // as to make TS think thast our empty object has AppStateContextProps type
+  {} as AppStateContextProps
+);
+
+type AppStateProviderProps = {
+  children?: React.ReactNode;
+};
+
+export const AppStateProvider = ({ children }: AppStateProviderProps) => {
+  const { lists } = appData;
+
+  const getTasksByListId = (id: string) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+
+  return (
+    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+// We don't need to specify the return type
+// Because TS can derive them automatically based on `AppStateContext` type
+export const useAppState = () => {
+  return useContext(AppStateContext);
+};
