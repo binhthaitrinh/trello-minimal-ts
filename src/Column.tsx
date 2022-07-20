ad31d15be88c0a0da2218ce0c5f9870c930aedaa
Kyle Trinh <kyle.trinh94@gmail.com>
2022-07-19 23:22:46 -0700

    :sparkles: Hide the original ofthe currently dragged item

--- src/Column.tsx
+++ src/Column.tsx
@@ -6,6 +6,7 @@ import { Card } from './Card';
 import { addTask, moveList } from './state/action';
 import { useAppState } from './state/AppStateContext';
 import { ColumnContainer, ColumnTitle } from './style';
+import { isHidden } from './utils/isHidden';
 import { useItemDrag } from './utils/useItemDrag';
 
 type ColumnProps = {
@@ -38,7 +39,7 @@ export const Column = ({ text, id }: ColumnProps) => {
   drag(drop(ref));
 
   return (
-    <ColumnContainer ref={ref}>
+    <ColumnContainer ref={ref} isHidden={isHidden(draggedItem, 'COLUMN', id)}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card text={task.text} key={task.id} id={task.id} />
